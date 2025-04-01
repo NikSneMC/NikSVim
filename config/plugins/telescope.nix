2b15f5efa20f16ebe175bf1f255a4086af537eb1
NikSne <commits@niksne.ru>
2025-04-01 16:28:17 +0300

    feat(plugins/telescope): add binds for more pickers

--- config/plugins/telescope.nix
+++ config/plugins/telescope.nix
@@ -2,9 +2,11 @@
   plugins.telescope = {
     enable = true;
     keymaps = {
+      "<leader>fb" = "buffers";
+      "<leader>fc" = "command_history";
+      "<leader>fd" = "diagnostics";
       "<leader>ff" = "find_files";
       "<leader>fg" = "live_grep";
-      "<leader>fb" = "buffers";
 
       "<leader>gc" = "git_commits";
       "<leader>gb" = "git_branches";
